a7a3021f8b17cd2192c423c37968c700c2c42648
gooseman1998 <sandergoose1@gmail.com>
2022-08-01 14:05:20 +0200

    Seperated hover function from change color function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,7 @@
 window.onload = function() {
   colorChange();
+  showGifOnHover();
+  svgLine();
 };
 
 
@@ -7,62 +9,7 @@ window.onload = function() {
 
 function colorChange() {
 
-    const ball = document.querySelector(".mouse-follower");
-    const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-    const mouse = { x: pos.x, y: pos.y };
-    const speed = 0.12;
-
-    const xSet = gsap.quickSetter(ball, "x", "px");
-    const ySet = gsap.quickSetter(ball, "y", "px");
-    
-    window.addEventListener("mousemove", e => {    
-      mouse.x = e.x;
-      mouse.y = e.y;  
-    });
-
-    // text hover show image/gif
-
-    let hoverWrapper =  document.querySelectorAll(".text-hover__wrapper");
-    let hoverGif  =  document.querySelectorAll(".text-hover__gif");
-
-    var image = new Image();
-    image.src = "./assets/images/gif/trains.gif";
-
-    hoverWrapper.forEach((wrapper) => {
-        wrapper.addEventListener("mouseover", () => {
-            let gif = wrapper.querySelector(".text-hover__gif");
-            gif.classList.add("text-hover__gif--active");
-            gif.setAttribute('src', image.src);
-        });
-
-        
-        wrapper.addEventListener("mouseout", () => {
-            gif = wrapper.querySelector(".text-hover__gif");
-            gif.classList.remove("text-hover__gif--active");
-        });
-    });
-    
-    gsap.ticker.add(() => {
-    
-        // adjust speed for higher refresh monitors
-        const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio()); 
-        
-        pos.x += (mouse.x - pos.x) * dt;
-        pos.y += (mouse.y - pos.y) * dt;
-        xSet(pos.x);
-        ySet(pos.y);
-
-        hoverGif.forEach((gif) => {
-            const xSetCircle = gsap.quickSetter(gif, "x", "px");
-            const ySetCircle = gsap.quickSetter(gif, "y", "px");
-
-            xSetCircle(pos.x - (gif.offsetWidth / 2));
-            ySetCircle(pos.y - (gif.offsetHeight / 2));
-        });
-    });
-
-
-    gsap.to('.color-change', {
+   gsap.to('.color-change', {
       scrollTrigger: {
           trigger:".color-change",
           toggleActions: "restart resume restart resume",
@@ -74,6 +21,7 @@ function colorChange() {
           onLeaveBack: changeColor,
       },
   });
+}
 
   function changeColor() {
       let hoverSections = document.querySelectorAll('.text-hover');
@@ -82,4 +30,62 @@ function colorChange() {
           section.classList.toggle('text-hover--dark');
       });
   }
+
+  
+function showGifOnHover() {
+
+  const ball = document.querySelector(".mouse-follower");
+  const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+  const mouse = { x: pos.x, y: pos.y };
+  const speed = 0.12;
+
+  const xSet = gsap.quickSetter(ball, "x", "px");
+  const ySet = gsap.quickSetter(ball, "y", "px");
+
+  window.addEventListener("mousemove", e => {    
+    mouse.x = e.x;
+    mouse.y = e.y;  
+  });
+
+  // text hover show image/gif
+
+  let hoverWrapper =  document.querySelectorAll(".text-hover__wrapper");
+  let hoverGif  =  document.querySelectorAll(".text-hover__gif");
+
+  var image = new Image();
+  image.src = "./assets/images/gif/trains.gif";
+
+  hoverWrapper.forEach((wrapper) => {
+    wrapper.addEventListener("mouseover", () => {
+        let gif = wrapper.querySelector(".text-hover__gif");
+        gif.classList.add("text-hover__gif--active");
+        gif.setAttribute('src', image.src);
+    });
+
+    
+    wrapper.addEventListener("mouseout", () => {
+        gif = wrapper.querySelector(".text-hover__gif");
+        gif.classList.remove("text-hover__gif--active");
+    });
+  });
+
+  gsap.ticker.add(() => {
+
+      // adjust speed for higher refresh monitors
+      const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio()); 
+      
+      pos.x += (mouse.x - pos.x) * dt;
+      pos.y += (mouse.y - pos.y) * dt;
+      xSet(pos.x);
+      ySet(pos.y);
+
+      hoverGif.forEach((gif) => {
+          const xSetCircle = gsap.quickSetter(gif, "x", "px");
+          const ySetCircle = gsap.quickSetter(gif, "y", "px");
+
+          xSetCircle(pos.x - (gif.offsetWidth / 2));
+          ySetCircle(pos.y - (gif.offsetHeight / 2));
+      });
+  });
+
 }
