8f64d795e3b7365529f4d680b513737f4371327f
gooseman1998 <sandergoose1@gmail.com>
2022-08-01 13:00:26 +0200

    Added color change functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,28 @@
 window.onload = function() {
-  ready();
+  colorChange();
 };
 
-function ready() {
-  console.log('Ready!');
+
+function colorChange() {
+
+    gsap.to('.color-change', {
+      scrollTrigger: {
+          trigger:".color-change",
+          toggleActions: "restart resume restart resume",
+          start:"top",
+          end: "bottom",
+          onEnter: changeColor,
+          onLeave: changeColor,
+          onEnterBack: changeColor,
+          onLeaveBack: changeColor,
+      },
+  });
+
+  function changeColor() {
+      let hoverSections = document.querySelectorAll('.text-hover');
+
+      hoverSections.forEach((section) => {
+          section.classList.toggle('text-hover--dark');
+      });
+  }
 }
